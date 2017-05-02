86988ff2c8585fca3f22f59b5e73baaf855284ba
theodinspire <bobryuu@gmail.com>
2017-05-02 12:11:40 -0500

    Initial Commit

---
+++ Markon-Markoff/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Markon-Markoff
+//
+//  Created by Eric Cormack on 5/2/17.
+//  Copyright © 2017 the Odin Spire. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
